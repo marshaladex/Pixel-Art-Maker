8dcc2aa7c31dceef1f10b30d0d21e1c1d6414c64
Adeyemo Yusuf <adeyemoyusuf07@gmail.com>
2022-05-01 07:49:21 +0100

    Udacity

---
+++ design.js
@@ -0,0 +1,89 @@
+// "use strict";
+
+// const $tableElement = $('#pixelCanvas');
+// const $inputHeight = $('#inputHeight');
+// const $inputWidth = $('#inputWidth');
+// const $colorPicker = $('#colorPicker');
+
+// $('#sizePicker').submit( event => {
+//     event.preventDefault();
+
+//     let width = $inputWidth.val();
+//     let height = $inputHeight.val();
+
+//     $tableElement.html(''); //clear
+
+//     makeGrid(height, width);
+//     addCellClickListener();
+// });
+
+// function makeGrid(height, width) {
+//     for(let i = 0; i < height; i++) {
+//         $tableElement.append('<tr></tr>');
+//     };
+
+//     for(let i = 0; i < width; i++) {
+//         $('tr').append('<td></td>');
+//     };
+// };
+
+// function addCellClickListener() {
+//     $('td').click( event => {
+//         let color = $colorPicker.val();
+//         $(event.currentTarget).css("background-color", color)
+//     });
+// };
+//imports useful elements, and assigns them to variables
+var canvas = document.getElementById("pixelCanvas");
+var color = document.getElementById("colorPicker");
+var sizePicker = document.getElementById("sizePicker");
+var height = document.getElementById("inputHeight");
+var width = document.getElementById("inputWidth");
+var toggle = document.getElementById("gridToggle");
+var save = document.getElementById("saveArt");
+
+//function to create grid and assign event listeners to all cells on creation
+function makeGrid(height, width) {
+    for (let y = 0; y < height; y++) {
+        let row = canvas.insertRow(y);
+        for (let x = 0; x < width; x++) {
+            let cell = row.insertCell(x);
+            cell.addEventListener("mousedown", function(evt) {
+                cell.style.backgroundColor = color.value;
+            cell.addEventListener("contextmenu", function(evt) {
+                evt.preventDefault();
+                cell.style.backgroundColor = "white";
+            } )
+            } )
+        }
+    }
+}
+
+//function to toggle on/off grid within the design canvas
+toggle.addEventListener("click", function() {
+    var elementsTd = document.getElementsByTagName("td");
+    var elementsTr = document.getElementsByTagName("tr");
+    for (var i=0; i<elementsTd.length; ++i) {
+        elementsTd[i].classList.toggle("off");
+    }
+    for (var j=0; j<elementsTr.length; ++j) {
+        elementsTr[j].classList.toggle("off");
+    }
+});
+
+//function that uses the size picker submit to call makeGrid() funtion
+sizePicker.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+    while (canvas.hasChildNodes()) {
+        canvas.removeChild(canvas.lastChild);
+    }
+    makeGrid(height.value, width.value);
+});
+
+//function that saves the art as canvas
+save.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    html2canvas(canvas).then(canvas => {
+        document.body.appendChild(canvas)
+    });
+});
